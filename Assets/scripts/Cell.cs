a5b28fbd6908e3db86cf0f70ca3e16e061731b07
Yuri Zinovev <fogugy@gmail.com>
2017-01-26 17:51:03 +0400

    config object

---
+++ Assets/scripts/Cell.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour {
+
+	CellState _state;
+	public CellState State { 
+		get{ return _state; } 
+		set { 
+			_state = value; 
+			Render();
+		} 
+	}
+
+	public Cell(){
+	}
+	public Cell(CellState state){
+		State = state;
+	}
+
+	void Render(){
+		
+	}
+}
+
+public enum CellState {
+	UP = 0,
+	RIGHT,
+	DOWN,
+	LEFT
+}
---
+++ Assets/scripts/ConfigPrefabs.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigPrefabs : MonoBehaviour{
+	public static string Yo;
+}
+
+public static class CellConfig {
+	public static string[] CellMaterials;
+	public static GameObject CellPrefab;
+}
