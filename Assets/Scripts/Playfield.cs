5a9f97ae97194e123781b2f65a8859bef8aeef34
Fogugy <fogugy@gmail.com>
2017-01-27 21:46:05 +0400

    random field fill

--- Assets/Scripts/Playfield.cs
+++ Assets/Scripts/Playfield.cs
@@ -2,42 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Playfield : MonoBehaviour {
+public class Playfield : MonoBehaviour
+{
 
-	public int FieldWidth { get; private set; }
-	GameObject[,] cells;
+    public int FieldWidth { get; private set; }
+    GameObject[,] cells;
 
-	void Start () {
-		Init ();
-	}
+    void Start()
+    {
+        Init();
+    }
 
-	void Init(){
-		FieldWidth = FieldConfig.FieldWidth;
-		cells = new GameObject[FieldWidth, FieldWidth];
+    void Init()
+    {
+        FieldWidth = FieldConfig.FieldWidth;
+        cells = new GameObject[FieldWidth, FieldWidth];
 
-		var initialOffset = FieldWidth / 2f;
-		var initialPos = new Vector3 (-initialOffset, 0f, -initialOffset);
+        var initialOffset = FieldWidth / 2f;
+        var initialPos = new Vector3(-initialOffset, 0f, -initialOffset);
+        var rnd = new System.Random();
 
-		for (short i = 0; i < FieldWidth; i++) {
-			for (short j = 0; j < FieldWidth; j++) {
-				cells [i, j] = Instantiate (CellConfig.Prefab);
-				cells [i, j].transform.position = initialPos + new Vector3 (1.1f * i, 0f, 1.1f * j);
+        for (short i = 0; i < FieldWidth; i++)
+        {
+            for (short j = 0; j < FieldWidth; j++)
+            {
+                cells[i, j] = Instantiate(CellConfig.Prefab);
+                cells[i, j].transform.position = initialPos + new Vector3(1.1f * i, 0f, 1.1f * j);
 
-				Cell cell = cells [i, j].GetComponent<Cell>();
-				cell.State = CellState.LEFT;
+                Cell cell = cells[i, j].GetComponent<Cell>();
+                CellState state;
+                
+                switch (rnd.Next(0, 5))
+                {
+                    case 0:
+                        state = CellState.LEFT;
+                        break;
+                    case 1:
+                        state = CellState.UP;
+                        break;
+                    case 2:
+                        state = CellState.RIGHT;
+                        break;
+                    case 3:
+                        state = CellState.DOWN;
+                        break;
+                    default: state = CellState.LEFT;
+                        break;
+                }
 
-				if (i % 3 == 0) {
-					cell.State = CellState.UP;
-				}
-
-				if (i % 5 == 0) {
-					cell.State = CellState.DOWN;
-				}
-
-				if ((j + i) % 5 == 0) {
-					cell.State = CellState.RIGHT;
-				}
-			}
-		}
-	}
+                cell.State = state;
+            }
+        }
+    }
 }
