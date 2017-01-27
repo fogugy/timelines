c5d96c92791e8cbd36f2a084dad4a18505ddc34f
Yuri Zinovev <fogugy@gmail.com>
2017-01-27 17:10:50 +0400

    simple field rendering

--- Assets/Scripts/Playfield.cs
+++ Assets/Scripts/Playfield.cs
@@ -13,12 +13,15 @@ public class Playfield : MonoBehaviour {
 
 	void Init(){
 		FieldWidth = FieldConfig.FieldWidth;
-
 		cells = new GameObject[FieldWidth, FieldWidth];
+
+		var initialOffset = FieldWidth / 2f;
+		var initialPos = new Vector3 (-initialOffset, 0f, -initialOffset);
 
 		for (int i = 0; i < FieldWidth; i++) {
 			for (int j = 0; j < FieldWidth; j++) {
 				cells [i, j] = Instantiate (CellConfig.Prefab);
+				cells [i, j].transform.position = initialPos + new Vector3 (1.1f * i, 0f, 1.1f * j);
 			}
 		}
 	}
