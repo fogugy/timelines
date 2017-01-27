7f18d1e03bc30754751ebb4b45b86ac45efc7633
Yuri Zinovev <fogugy@gmail.com>
2017-01-27 18:46:58 +0400

    field filling

--- Assets/scripts/Cell.cs
+++ Assets/scripts/Cell.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Cell : MonoBehaviour {
 
+	static Dictionary<CellState, Material> _materials = new Dictionary<CellState, Material> ();
+
 	CellState _state;
 	public CellState State { 
 		get{ return _state; } 
@@ -14,13 +16,19 @@ public class Cell : MonoBehaviour {
 	}
 
 	public Cell(){
-	}
-	public Cell(CellState state){
-		State = state;
+		if (_materials.Count != 0) {
+			return;
+		}
+
+		short i = 0;
+		foreach (var mat in CellConfig.Materials) {
+			_materials.Add((CellState)i++, mat);
+		}
 	}
 
 	void Render(){
-		
+		var rnd = GetComponent<Renderer> ();
+		rnd.material = _materials [State];
 	}
 }
 
